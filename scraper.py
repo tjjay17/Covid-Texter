e45bfe969e484862288ff2096567b47b664d68ae
Thanooj <thanoojjay@gmail.com>
2020-11-24 02:29:01 -0500

    added phone number array

--- scraper.py
+++ scraper.py
@@ -4,13 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import io
-import requests
 #from webdriver_manager.chrome import ChromeDriverManager
 from twilio.rest import Client
 
 DRIVER_PATH = 'C:\\Users\\thano\\chromedriver'
+ACCOUNT_SID = 'AC036c032f9041a70fc94fcea7012c2a13'
+AUTH_TOKEN = '0e437fb716f470d68a8af34ad67c2d87'
 
-#driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+#array of numbers go in here as strings
+PHONE_NUMBERS = []
 
 #this is gui; actually opens the browser
 #driver.get('https://google.ca')
@@ -24,25 +26,24 @@ options.add_argument('--disable-notifications')
 driver = webdriver.Chrome(options = options,executable_path=DRIVER_PATH)
 driver.get('https://covid-19.ontario.ca/data')
 
-tableButton = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div/div[1]/button[2]')
+tableButton = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div[1]/button[2]')
 tableButton.click()
 
-dateElement = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[1]')
-casesTodayElement = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[2]')
-
-date = dateElement.get_attribute('innerHTML')
+#dateElement = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[1]')
+casesTodayElement = driver.find_element_by_xpath('//*[@id="CasesDaily"]/div/div[2]/div/div/div/div/div/table/tbody/tr[2]/td[2]')
 casesToday = casesTodayElement.get_attribute('innerHTML')
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 #The twilio number goes in from and to is the number that is wanted to receive msg
-message = client.messages \
-    .create(
-         body=date + " " + casesToday + ' Cases Today.',
-         from_='twilio number here',
-         to='put recepient phone number here'
-     )
+#Loop through array of numbers to send numbers
+for i in range (len(PHONE_NUMBERS)):
+    message = client.messages \
+        .create(
+            body = casesToday + ' Cases Today.',
+            from_ = '+14305411019',
+            to = PHONE_NUMBERS[i]
+        )
 
 print(message.sid)
-
 driver.quit()
